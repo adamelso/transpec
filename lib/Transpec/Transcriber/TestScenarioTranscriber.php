33f9b4a8c29f161ccc2ee8445ebe9999c25dfbf1
Adam Elsodaney <adam.elso@gmail.com>
2023-06-25 19:44:15 +0100

    Implement conversion for shouldThrow() to expectException() for FQCN of exceptions.

--- lib/Transpec/Transcriber/TestScenarioTranscriber.php
+++ lib/Transpec/Transcriber/TestScenarioTranscriber.php
@@ -41,32 +41,97 @@ class TestScenarioTranscriber implements Transcriber
         $transNode->flags = BuilderHelpers::addModifier($transNode->flags, Node\Stmt\Class_::MODIFIER_PUBLIC);
         $transNode->returnType = new Node\Name('void');
 
+        $newStatements = [];
+
         foreach ($transNode->stmts as $stmt) {
             if (! $stmt instanceof Node\Stmt\Expression) {
+                $newStatements[] = $stmt;
                 continue;
             }
 
             // $this->subjectMethod( $exampleArgs )
             if (! $stmt->expr instanceof Node\Expr\MethodCall) {
+                $newStatements[] = $stmt;
                 continue;
             }
 
             $assertionCall = $stmt->expr;
             $subjectCall = $stmt->expr->var;
 
-            if ('shouldReturn' === $assertionCall->name->name && $subjectCall instanceof Node\Expr\MethodCall) {
-                $expected = $assertionCall->args;
-                // $actual = $subjectCall->args;
+            if (!$subjectCall instanceof Node\Expr\MethodCall) {
+                $newStatements[] = $stmt;
+                continue;
+            }
 
-                $subjectCall->var = new Node\Expr\PropertyFetch(new Node\Expr\Variable('this'), 'subject');
+            switch ($assertionCall->name->name) {
+                // $this->subjectMethod( $exampleArgs )->shouldReturn( $expectedResult );
+                // $this->subjectMethod( $exampleArgs )->shouldBe( $expectedResult );
+                case 'shouldReturn':
+                case 'shouldBe':
 
-                $stmt->expr = $this->builderFactory->staticCall('static', 'assertEquals', [
-                    $expected[0],
-                    $subjectCall
-                ]);
+                    [$n] = $this->rewriteAssertion($assertionCall, $subjectCall);
+                    $stmt->expr = $n;
+                    $newStatements[] = $stmt;
+
+                    break;
+
+                // $this->shouldThrow( \Exception::class )->duringSubjectMethod( $exampleArgs );
+                default:
+                    if (str_starts_with($assertionCall->name->name, 'during') && 'shouldThrow' === $subjectCall->name->name) {
+                        // Note the subject and assertion calls are swapped round.
+                        $left = $subjectCall;
+                        $right = $assertionCall;
+                        $newStatements = array_merge($newStatements, $this->rewriteExceptionAssertion($left, $right));
+                        break;
+                    }
+                    $newStatements[] = $stmt;
             }
         }
 
+        $transNode->stmts = $newStatements;
+
         return $transNode;
+    }
+
+    private function rewriteAssertion(Node\Expr\MethodCall $assertionCall, Node\Expr\MethodCall $subjectCall): array
+    {
+        $expected = $assertionCall->args;
+        // $actual = $subjectCall->args;
+
+        $subjectCall->var = new Node\Expr\PropertyFetch(new Node\Expr\Variable('this'), 'subject');
+
+        return [
+            $this->builderFactory->staticCall('static', 'assertEquals', [
+                $expected[0],
+                $subjectCall
+            ])
+        ];
+    }
+
+    private function rewriteExceptionAssertion(Node\Expr\MethodCall $assertionCall, Node\Expr\MethodCall $subjectCall): array
+    {
+        $expected = $assertionCall->args;
+        $actual = $subjectCall->args;
+
+        $methodName = lcfirst(substr($subjectCall->name, strlen('during')));
+
+        $expectException = new Node\Stmt\Expression(
+            new Node\Expr\MethodCall(new Node\Expr\Variable('this'), 'expectException', [
+                $expected[0],
+            ])
+        );
+
+        $callTestSubject = new Node\Stmt\Expression(
+            new Node\Expr\MethodCall(
+                new Node\Expr\PropertyFetch(new Node\Expr\Variable('this'), 'subject'),
+                $methodName,
+                $actual
+            )
+        );
+
+        return [
+            $expectException,
+            $callTestSubject,
+        ];
     }
 }
